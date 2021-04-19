60502394980f94233b46419e5913660564f01c83
Jouni Juntunen <jouni.juntunen@oamk.fi>
2021-04-19 18:32:42 +0300

    Finished example.

--- App.js
+++ App.js
@@ -1,12 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useState} from 'react';
+import { Platform, Pressable, StyleSheet, Text, View } from 'react-native';
+import DateTimePicker from '@react-native-community/datetimepicker';
 
 export default function App() {
+  const [date, setDate] = useState(new Date()); // State variable for the data.
+  const [show,setShow] = useState(false); // State variable, that controls, if calendar is visible or not.
+
+  // Method handles change of date.
+  const onChange = (_, selectedDate) => {
+    if (Platform.OS === 'ios') { 
+      setShow(false);
+    }
+
+    const currentDate = selectedDate || date;
+    setDate(currentDate);
+  };
+
+  const toggle = () => {
+    setShow(prevShow => !prevShow);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {show && Platform.OS === 'ios' &&  (
+       <DateTimePicker
+          style={{width: 320}}
+          mode={'date'}
+          display="inline"
+          value={date}
+          onChange={onChange}
+        
+        />
+      )}
+      {show && Platform.OS === 'android' &&  (
+       <DateTimePicker
+          mode={'date'}
+          display="default"
+          value={date}
+          onChange={onChange}
+        />
+      )}
+
+      <Pressable 
+        onPress={toggle}>
+        <Text>
+          {date.getDate()}.{date.getMonth() + 1}.{date.getFullYear()}
+        </Text>
+      </Pressable>
     </View>
   );
 }
@@ -15,7 +55,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    marginTop: 50,
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
 });
